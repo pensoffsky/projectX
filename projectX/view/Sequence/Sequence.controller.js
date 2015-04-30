3ac266bb49e76cebbc6f2018c22aa6bd2ed8d3da
Kuesters, Elmar <elmar.kuesters@sap.com>
2015-04-30 23:53:33 +0200

    moved testrun functionality to sequence screen
    Signed-off-by: Kuesters, Elmar <elmar.kuesters@sap.com>

--- projectX/view/Sequence/Sequence.controller.js
+++ projectX/view/Sequence/Sequence.controller.js
@@ -19,10 +19,14 @@ projectX.util.Controller.extend("projectX.view.Sequence.Sequence", {
 	// /////////////////////////////////////////////////////////////////////////////
 
 	onInit : function() {
-		this._localProjectModel = new sap.ui.model.json.JSONModel({
-			
+		this._localUIModel = new sap.ui.model.json.JSONModel();
+		this._localUIModel.setData({
+			configVisible: true,
+			testVisible: false,
+			testRunning : false,
+			testNotRunning : true
 		});
-		this.getView().setModel(this._localProjectModel, "localUIModel");
+		this.getView().setModel(this._localUIModel, "localUIModel");
 		
 		//hook navigation event
 		this.getRouter().getRoute("sequence").attachPatternMatched(this.onRouteMatched, this);
@@ -42,38 +46,54 @@ projectX.util.Controller.extend("projectX.view.Sequence.Sequence", {
 			this._oOriginalSequence = oSelectedSequence;
 			this._oSequence = jQuery.extend(true, [], oSelectedSequence);
 			//set data from selected sequence into local project model
-			this._localProjectModel.setProperty("/sequence", this._oSequence);
+			this._localUIModel.setProperty("/sequence", this._oSequence);
 			//set helper values to modify the page between edit and new
-			this._localProjectModel.setProperty("/reasonNew", false);
-			this._localProjectModel.setProperty("/reasonEdit", true);
-			this._localProjectModel.setProperty("/reason", "Edit sequence");
+			this._localUIModel.setProperty("/reasonNew", false);
+			this._localUIModel.setProperty("/reasonEdit", true);
+			this._localUIModel.setProperty("/reason", "Edit sequence");
 			
 			//get requests for requestIds and set to local ui model
 			var aSelectedRequests = this._oSequence.getRequestIds().map(function(iId){
 				return oSelectedProject.getRequestByIdentifier(iId);
 			});
-			this._localProjectModel.setProperty("/selectedRequests", aSelectedRequests);
+			this._localUIModel.setProperty("/selectedRequests", aSelectedRequests);
 		} else/* if (sReason === "new")*/{
 			this._oSequence = new projectX.util.Sequence({
 				name : "new sequence"
 			});
 			//user wants to create a new sequence
 			//clear the local project model
-			this._localProjectModel.setProperty("/sequence", this._oSequence);
+			this._localUIModel.setProperty("/sequence", this._oSequence);
 			//set helper values to modify the page between edit and new
-			this._localProjectModel.setProperty("/reasonNew", true);
-			this._localProjectModel.setProperty("/reasonEdit", false);
-			this._localProjectModel.setProperty("/reason", "Create new sequence");
-			this._localProjectModel.setProperty("/selectedRequests",[]);
+			this._localUIModel.setProperty("/reasonNew", true);
+			this._localUIModel.setProperty("/reasonEdit", false);
+			this._localUIModel.setProperty("/reason", "Create new sequence");
+			this._localUIModel.setProperty("/selectedRequests",[]);
 		}
 		
 		//add list of requests to local ui model
-		this._localProjectModel.setProperty("/requests", oSelectedProject.getRequests());
+		this._localUIModel.setProperty("/requests", oSelectedProject.getRequests());
 	},
 
 	// /////////////////////////////////////////////////////////////////////////////
 	// /// event handler
 	// /////////////////////////////////////////////////////////////////////////////
+
+	onSegmentedButtonSelect : function(oEvent){
+		var sSelectedId = oEvent.getParameter("id");
+		switch (sSelectedId) {
+			case this.createId("idButtonConfig"):
+				this._localUIModel.setProperty("/configVisible", true);
+				this._localUIModel.setProperty("/testVisible", false);
+				break;
+			case this.createId("idButtonTest"):
+				this._localUIModel.setProperty("/configVisible", false);
+				this._localUIModel.setProperty("/testVisible", true);
+				break;
+		default:
+			console.log("problem with segmented button on sequence page");
+		}
+	},
 
 	onSave : function() {
 		//basic input validation
@@ -83,10 +103,9 @@ projectX.util.Controller.extend("projectX.view.Sequence.Sequence", {
 		}
 		
 		this._oOriginalSequence.setName(this._oSequence.getName());
-		var aRequests = this._localProjectModel.getProperty("/selectedRequests");
+		var aRequests = this._localUIModel.getProperty("/selectedRequests");
 		this._oOriginalSequence.addRequestIds(aRequests);
-		var oModel = this.getView().getModel();
-		oModel.updateBindings();
+		this._localUIModel.updateBindings();
 	},
 	
 	onCreate : function() {
@@ -118,9 +137,9 @@ projectX.util.Controller.extend("projectX.view.Sequence.Sequence", {
 		}
 		
 		var oRequest = oSelectedItem.getBindingContext("localUIModel").getObject();
-		var aSelectedRequests = this._localProjectModel.getProperty("/selectedRequests");
+		var aSelectedRequests = this._localUIModel.getProperty("/selectedRequests");
 		aSelectedRequests.push(oRequest);
-		this._localProjectModel.setProperty("/selectedRequests",aSelectedRequests);
+		this._localUIModel.setProperty("/selectedRequests",aSelectedRequests);
 	},
 	
 	/**
@@ -134,16 +153,125 @@ projectX.util.Controller.extend("projectX.view.Sequence.Sequence", {
 		}
 		
 		var oRequest = oSelectedItem.getBindingContext("localUIModel").getObject();
-		var aSelectedRequests =  this._localProjectModel.getProperty("/selectedRequests");
+		var aSelectedRequests =  this._localUIModel.getProperty("/selectedRequests");
 		aSelectedRequests.splice(aSelectedRequests.indexOf(oRequest), 1);
-		this._localProjectModel.setProperty("/selectedRequests",aSelectedRequests);
+		this._localUIModel.setProperty("/selectedRequests",aSelectedRequests);
 		this._oTable.removeSelections(true);
 	},
 
-	// /////////////////////////////////////////////////////////////////////////////
-	// /// private methods
-	// /////////////////////////////////////////////////////////////////////////////
+	/**
+	* execute the first request. when it finished than execute the next request.
+	* can be aborted by the user. (see onStopSequence function)
+	*/
+	onRunSequence : function() {
+		//get the requests
+		var aRequests = this._localUIModel.getProperty("/selectedRequests");
+		if (!aRequests || aRequests.length <= 0) {
+			return;
+		}
+		//TODO create deep copy of single requests
+		//maybe on entering the screen
+		
+		//clear existing results
+		this.onClearResults();
+		
+		this._setRunning(true);
+		this._executeRequests(0, aRequests);
+	},
+
+	/**
+	* stop the currently running sequence.
+	* works only when sequence was started with onRunSequence function.
+	*/
+	onStopSequence : function() {
+		if (this._getRunning()) {
+			this._bAbortSequence = true;
+		}
+	},
 	
+	/**
+	* loop over the requests and clear out the result data from the last test run.
+	*/
+	onClearResults : function() {
+		//get the requests
+		var aRequests = this._localUIModel.getProperty("/selectedRequests");
+		if (!aRequests || aRequests.length <= 0) {
+			return;
+		}
+		
+		//loop over requests and reset the result data
+		for (var i = 0; i < aRequests.length; i++) {
+			//clear old request results
+			aRequests[i].resetTempData();
+		}
+		this._localUIModel.updateBindings();
+	},
+	
+	/**
+	* the user clicked on the title of a specific request.
+	*/
+	onRequestNamePress : function (oEvent) {
+		//TODO what logic to implement here?
+		//navigate to the request?
+		//show details for the request?
+		//show the result of the assertions (not yet implemented)
+		var oParameters = oEvent.getParameters();
+		// var oItem = this.getView().byId(sId);
+		var oObjectHeader = oEvent.getSource();
+		var oColumnListItem = oObjectHeader.getParent(); //the columListItem control
+		oColumnListItem.toggleStyleClass("columnListItemExpanded");
+	},
+	
+	// /////////////////////////////////////////////////////////////////////////////
+	// /// Private Methods
+	// /////////////////////////////////////////////////////////////////////////////
+
+	/**
+	* use recursion to go through the request and execute them one by one.
+	* @param  {int} iIndex    the index of the request to execute next
+	* @param  {array} aRequests array of all requests to execute one after another
+	*/
+	_executeRequests : function(iIndex, aRequests) {
+		var that = this;		
+		if (aRequests.length <= iIndex){
+			//abort
+			this._setRunning(false);
+			return;
+		}
+		
+		//execute the request
+		var oRequest = aRequests[iIndex];
+		var oDeferred = oRequest.execute();
+		//add hanlder that gets called once the request finishes
+		oDeferred.always(function() {
+			oRequest.checkAssertions();
+			//update bindings so that the status will be displayed
+			that._localUIModel.updateBindings(false);
+			
+			if (that._bAbortSequence === true){
+				that._bAbortSequence = false;
+				that._setRunning(false);
+				return;
+			}
+			
+			that._executeRequests(iIndex + 1, aRequests);
+			//TODO add positiliy to abort on failure
+		});		
+	},
+
+	/**
+	* set the ui bindings to "test is running" or "test is not running".
+	* used to change visibilites on the UI.
+	* @param {boolean} bIsRunning state of test running
+	*/
+	_setRunning : function(bIsRunning) {
+		this._localUIModel.setProperty("/testRunning", bIsRunning);
+		this._localUIModel.setProperty("/testNotRunning", !bIsRunning);
+	},
+	
+	_getRunning : function() {
+		return this._localUIModel.getProperty("/testRunning");
+	}
 	
 
 });
