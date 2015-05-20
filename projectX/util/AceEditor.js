59da945b2f5b9f063340c90a24e5d7478fdace49
Pensoffsky <pensoffsky@gmx.net>
2015-05-20 23:29:02 +0200

    fixed aceeditor unneeded rerenders

--- projectX/util/AceEditor.js
+++ projectX/util/AceEditor.js
@@ -1,108 +1,135 @@
 sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control'],
-    function(jQuery, Control) {
-        "use strict";
+	function(jQuery, Control) {
+		"use strict";
 
-        var AceEditor = Control.extend("projectX.util.AceEditor", {
-            metadata: {
+		var AceEditor = Control.extend("projectX.util.AceEditor", {
+			metadata: {
 
-                properties: {
-               
-			        width : {type : "sap.ui.core.CSSSize", group : "Appearance", defaultValue : null},
-	
-			        height : {type : "sap.ui.core.CSSSize", group : "Appearance", defaultValue : null},
-                    
-                    value: {
-                        type: "string",
-                        defaultValue: null
-                    },
-                    
-                    readOnly: {
-                        type: "boolean",
-                        defaultValue: false
-                    },
-                    
-                    enableWrapMode: {
-                        type: "boolean",
-                        defaultValue: false
-                    },
-                    
-                    autoHeightMode: {
-                        type: "boolean",
-                        defaultValue: false
-                    },
-                    
-                    mode: {
-                        type: "string",
-                        defaultValue: "javascript"
-                    }, 
-                },
-                events: {
-			        someEvent : {}
-                }
-            }
-        });
+				properties: {
 
-// /////////////////////////////////////////////////////////////////////////////
-// /// Control Overrides
-// /////////////////////////////////////////////////////////////////////////////
+					width: {
+						type: "sap.ui.core.CSSSize",
+						group: "Appearance",
+						defaultValue: null
+					},
 
-        AceEditor.prototype.init = function() {
-            console.log("aceeditor init");
-        };
+					height: {
+						type: "sap.ui.core.CSSSize",
+						group: "Appearance",
+						defaultValue: null
+					},
 
-        
-        AceEditor.prototype.onAfterRendering = function() {
-            console.log("aceeditor onAfterRendering");
+					value: {
+						type: "string",
+						defaultValue: null
+					},
+
+					readOnly: {
+						type: "boolean",
+						defaultValue: false
+					},
+
+					enableWrapMode: {
+						type: "boolean",
+						defaultValue: false
+					},
+
+					autoHeightMode: {
+						type: "boolean",
+						defaultValue: false
+					},
+
+					mode: {
+						type: "string",
+						defaultValue: "javascript"
+					},
+				},
+				events: {
+					someEvent: {}
+				}
+			}
+		});
+
+		// /////////////////////////////////////////////////////////////////////////////
+		// /// Control Overrides
+		// /////////////////////////////////////////////////////////////////////////////
+
+		AceEditor.prototype.init = function() {
+			console.log("aceeditor init");
+		};
+
+
+		AceEditor.prototype.onAfterRendering = function() {
+			console.log("aceeditor onAfterRendering");
 			var sEditID = this.getId();
-            if(this._editor){
-                this._editor.destroy();
-            }
-                
-            
+			if (this._editor) {
+				this._editor.destroy();
+			}
+
 			this._editor = ace.edit(sEditID);
 			this._editor.setTheme("ace/theme/tomorrow");
 			this._editor.getSession().setMode("ace/mode/" + this.getMode());
-    
-            this._editor.setReadOnly(this.getReadOnly());
-            
-            if (this.getAutoHeightMode()) {
-                this._editor.setOptions({
-                    maxLines: Infinity
-                });
-            }
-            
-            this._editor.getSession().setUseWrapMode(this.getEnableWrapMode());
-            
-            this._editor.setValue(this.getValue(), 1);
-            
-            var that = this;
-            this._editor.getSession().on('change', function(e) {
-                var sEditorValue = that._editor.getValue();
-                var sControlValue = that.getValue();
-                if (sEditorValue !== sControlValue) {
-                    that.setProperty("value", sEditorValue, true);
-                }
-                
+
+			this._editor.setReadOnly(this.getReadOnly());
+
+			if (this.getAutoHeightMode()) {
+				this._editor.setOptions({
+					maxLines: Infinity
+				});
+			}
+
+			this._editor.getSession().setUseWrapMode(this.getEnableWrapMode());
+
+			this._editor.setValue(this.getValue(), 1);
+
+			var that = this;
+			this._editor.getSession().on('change', function(e) {
+				var sEditorValue = that._editor.getValue();
+				var sControlValue = that.getValue();
+				if (sEditorValue !== sControlValue) {
+					that.setProperty("value", sEditorValue, true);
+				}
+
 			});
-        };
+		};
 
-        AceEditor.prototype.setValue = function(sValue) {  
-            this.setProperty("value", sValue);
-        };
-        
-        
-// /////////////////////////////////////////////////////////////////////////////
-// /// Public functions
-// /////////////////////////////////////////////////////////////////////////////
-        
+		/**
+		 * update the value property but do not trigger a rerender of the control.
+		 * instead directly set the value into the ace editor
+		 * @param {string} sValue the new value string
+		 */
+		AceEditor.prototype.setValue = function(sValue) {
+			this.setProperty("value", sValue, true);
+      if (this._editor) {
+        this._editor.setValue(sValue, 1);
+      }
+		};
     
-        
-// /////////////////////////////////////////////////////////////////////////////
-// /// Private functions
-// /////////////////////////////////////////////////////////////////////////////
+    /**
+     * update the mode property but do not trigger a rerender of the control.
+     * instead directly set the value into the ace editor
+     * @param {string} sMode the new mode of the ace editor
+     */
+    AceEditor.prototype.setMode = function(sMode) {
+      this.setProperty("mode", sMode, true);
+      if (this._editor) {
+        this._editor.getSession().setMode("ace/mode/" + sMode);
+      }
+    };
 
-    
 
-        return AceEditor;
+		// /////////////////////////////////////////////////////////////////////////////
+		// /// Public functions
+		// /////////////////////////////////////////////////////////////////////////////
 
-    }, /* bExport= */ true);
+
+
+		// /////////////////////////////////////////////////////////////////////////////
+		// /// Private functions
+		// /////////////////////////////////////////////////////////////////////////////
+
+
+
+		return AceEditor;
+
+	}, /* bExport= */ true);
