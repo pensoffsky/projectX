216b93c3809b34a829844e048f7db3780ad95853
Steffen Froehlich <steffenfroehlich@gmx.de>
2016-02-28 17:59:54 +0100

    code style: replaced single quotes to double quotes

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,14 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     browserSync: {
       bsFiles: {
         src : [
-          'projectX/**/*.js',
-          'projectX/**/*.xml',
-          'projectX/**/*.css',
-          'tests/**/*.js'
+          "projectX/**/*.js",
+          "projectX/**/*.xml",
+          "projectX/**/*.css",
+          "tests/**/*.js"
         ]
       },
       options: {
@@ -35,8 +35,8 @@ module.exports = function(grunt) {
     //automatic compile when they change
     watch: {
       styles: {
-        files: ['projectX/**/*.less'], // which files to watch
-        tasks: ['less'],
+        files: ["projectX/**/*.less"], // which files to watch
+        tasks: ["less"],
         options: {
           nospawn: true
         }
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
         options: {
           port: 9001,
           useAvailablePort: true,
-          base: './',
+          base: "./",
           keepalive: false
         }
       },
@@ -67,15 +67,15 @@ module.exports = function(grunt) {
 	  main: {
 		  files: [
 	      // indexElectron.hml
-	      {src: ['indexElectron.html'], dest: 'build/release/indexElectron.html', filter: 'isFile'},
+	      {src: ["indexElectron.html"], dest: "build/release/indexElectron.html", filter: "isFile"},
 	      // electron.js
-	      {src: ['electron.js'], dest: 'build/release/electron.js', filter: 'isFile'},
+	      {src: ["electron.js"], dest: "build/release/electron.js", filter: "isFile"},
 	      // package.json
-	      {src: ['package.json'], dest: 'build/release/package.json', filter: 'isFile'},
+	      {src: ["package.json"], dest: "build/release/package.json", filter: "isFile"},
           //files
-          {expand: true, cwd: 'projectX/', src: ['**'], dest: 'build/release/projectX'},
-          {expand: true, cwd: '3rdparty/', src: ['**'], dest: 'build/release/3rdparty'},
-          {expand: true, cwd: 'resources/', src: ['**'], dest: 'build/release/resources'}
+          {expand: true, cwd: "projectX/", src: ["**"], dest: "build/release/projectX"},
+          {expand: true, cwd: "3rdparty/", src: ["**"], dest: "build/release/3rdparty"},
+          {expand: true, cwd: "resources/", src: ["**"], dest: "build/release/resources"}
 	    ]
 	  },
       "sap.ui.core": {
@@ -148,111 +148,108 @@ module.exports = function(grunt) {
             files: [
                 // index.hml
                 {
-                    src: ['index.html'],
-                    dest: 'buildGw/release/index.html',
-                    filter: 'isFile'
+                    src: ["index.html"],
+                    dest: "buildGw/release/index.html",
+                    filter: "isFile"
                 },
                 //css, i18n, util, view folders
                 {
-                    cwd: 'projectX',
-                    src: ['**/*'],
+                    cwd: "projectX",
+                    src: ["**/*"],
                     dots: true,
                     expand: true,
-                    dest: 'buildGw/release/projectX/'
+                    dest: "buildGw/release/projectX/"
                 },
                 //Component: route matching
                 {
-                    src: ['projectX/Component.js', 'projectX/MyRouter.js'],
-                    dest: 'buildGw/release/projectX/',
-                    filter: 'isFile'
+                    src: ["projectX/Component.js", "projectX/MyRouter.js"],
+                    dest: "buildGw/release/projectX/",
+                    filter: "isFile"
                 },
                 //3rd party components
                 {
-                    cwd: '3rdparty',
-                    src: ['**/*'],
+                    cwd: "3rdparty",
+                    src: ["**/*"],
                     dots: false,
                     expand: true,
-                    dest: 'buildGw/release/3rdparty/'
+                    dest: "buildGw/release/3rdparty/"
                 }
             ] // copy:buildGw:files
         } // copy:buildGw
 	},
-    'build-electron-app': {
+    "build-electron-app": {
         options: {
-            platforms: ['darwin', 'win32'],
+            platforms: ["darwin", "win32"],
             app_dir: "./build/release",
             build_dir: "./build/electron"
         }
     },
     // clean tasks
     clean: {
-        build: ['build'],
-        buildGw: ['buildGw']
+        build: ["build"],
+        buildGw: ["buildGw"]
     },
     // zip tasks
     zip: {
-        'buildGw': {
+        "buildGw": {
             cwd: "buildGw/release/",
-            src: ['buildGw/release/**'],
-            dest: 'release/projectX-Gateway.zip'
+            src: ["buildGw/release/**"],
+            dest: "release/projectX-Gateway.zip"
         },
-        'buildElectronDarwin': {
+        "buildElectronDarwin": {
             cwd: "build/Electron/",
-            src: ['build/Electron/darwin/**'],
-            dest: 'release/projectX-electron-mac.zip'
+            src: ["build/Electron/darwin/**"],
+            dest: "release/projectX-electron-mac.zip"
         },
-        'buildElectronWin32': {
+        "buildElectronWin32": {
             cwd: "build/Electron/",
-            src: ['build/Electron/win32/**'],
-            dest: 'release/projectX-electron-win32.zip'
+            src: ["build/Electron/win32/**"],
+            dest: "release/projectX-electron-win32.zip"
         }
     }
   });
 
-  // Load the plugin that provides the "browserSync" task.
-  grunt.loadNpmTasks('grunt-browser-sync');
-  // Load the plugin that provides the "less" task.
-  grunt.loadNpmTasks('grunt-contrib-less');
-  // Load the plugin that provides the "watch" task.
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  // Load the plugin that provides the "connect" task.
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  // Load the plugin that provides the "copy" task.
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  // Load the plugin that provides the "build-electron-app" task
-  grunt.loadNpmTasks('grunt-electron-app-builder');
-  // Load the plugin that provides the clean task
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  // Load the plugin that provides the zip task
-  grunt.loadNpmTasks('grunt-zip');
+  // load all needed plugins via package.json 'devDependencies'
+  Object.keys(grunt.config.get("pkg").devDependencies).forEach(function(dependency) {
+      if (/^grunt(?!(-cli)?$)/.test(dependency)) {  // ignore grunt and grunt-cli
+        grunt.loadNpmTasks(dependency);
+      }
+  });
 
   // Default task for development, run http servers and less compiler
-  grunt.registerTask('default', ['browserSync', 'connect:server', 'watch']);
+  grunt.registerTask("default", [
+      "browserSync",
+      "connect:server",
+      "watch"
+  ]);
 
   // build tasks for Gateway package
-  grunt.registerTask('buildGw', [
-      'clean:buildGw',
-      'copy:buildGw',
-      'zip:buildGw',
-      'connect:buildGwServer' //start a server here so that the gw can pull the zip file from it
+  // after zipping the build, a simple HTTP server is started, from where it can
+  // be uploaded to a Gateway server via
+  // /UI5/UI5_REPOSITORY_LOAD_[HTTP|HTTPN] transaction
+  grunt.registerTask("buildGw", [
+      "clean:buildGw",
+      "copy:buildGw",
+      "zip:buildGw",
+      "connect:buildGwServer"
   ]);
 
   // build the electron shell app for win and os x, create zip and copy to release folder on root
-  grunt.registerTask('build', [
-      'clean:build',
-      'copy:main',
-      'build-electron-app',
-      'zip:buildElectronDarwin',
-      'zip:buildElectronWin32'
+  grunt.registerTask("build", [
+      "clean:build",
+      "copy:main",
+      "build-electron-app",
+      "zip:buildElectronDarwin",
+      "zip:buildElectronWin32"
   ]);
 
   // after bower install copy the resources from the bower folder into the resources folder
-  grunt.registerTask('copyresources', [
-      'copy:sap.ui.core', 
-      'copy:sap.ui.layout', 
-      'copy:sap.ui.table', 
-      'copy:sap.ui.unified', 
-      'copy:sap.m', 
-      'copy:bluecrystal'
+  grunt.registerTask("copyresources", [
+      "copy:sap.ui.core",
+      "copy:sap.ui.layout",
+      "copy:sap.ui.table",
+      "copy:sap.ui.unified",
+      "copy:sap.m",
+      "copy:bluecrystal"
   ]);
 };
