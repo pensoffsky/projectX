b77009cb6594cfed2f4f067e33576f7cbfe33546
Kuesters, Elmar <pensoffsky@gmx.net>
2016-02-28 11:56:43 +0100

    implemented a few unit tests
    added sinon files to test html

--- tests/unit/util/Assertion.js
+++ tests/unit/util/Assertion.js
@@ -2,7 +2,13 @@ jQuery.sap.require("projectX.util.Assertion");
 
 QUnit.module("util/Assertion", {
     setup: function() {
-      this._oAssertion = new projectX.util.Assertion();
+      this._oAssertion = new projectX.util.Assertion({
+          "name": "",
+          "assertProperty": "STATUS",
+          "operation": "EQUALS",
+          "path": "",
+          "expected": "201"
+        });
     },
     teardown: function() {
 			
@@ -21,55 +27,96 @@ QUnit.test("createDefaultAssertion", 1, function(assert) {
 });
 
 
-QUnit.test("resetTempData", 1, function(assert) {
+QUnit.test("resetTempData", 3, function(assert) {
+    //set temporary data
+    this._oAssertion.setEvaluatedValue("string1");
+    this._oAssertion.setResult(true);
+    this._oAssertion.setResultReady(true);
+    
 	this._oAssertion.resetTempData();
+    
+    //check that data was resetted to default values
+    assert.equal(this._oAssertion.getEvaluatedValue(), "","EvaluatedValue is empty");
+    assert.equal(this._oAssertion.getResult(), false,"Result is empty");
+    assert.equal(this._oAssertion.getResultReady(), false,"ResultReady is empty");
 });
 
-QUnit.test("serialize", 1, function(assert) {
-	this._oAssertion.serialize();
+QUnit.test("serialize", 5, function(assert) {
+	var oSerialized = this._oAssertion.serialize();
+    assert.equal(oSerialized.name, this._oAssertion.getName(), "name serialized");
+    assert.equal(oSerialized.assertProperty, this._oAssertion.getAssertProperty(), "assertProperty serialized");
+    assert.equal(oSerialized.operation, this._oAssertion.getOperation(), "operation serialized");
+    assert.equal(oSerialized.path, this._oAssertion.getPath(), "path serialized");
+    assert.equal(oSerialized.expected, this._oAssertion.getExpected(), "expected serialized");
 });
 
-QUnit.test("assert", 1, function(assert) {
-	this._oAssertion.assert();
+QUnit.test("assert", 3, function(assert) {
+    var iStatus = 200;
+    var sResponseBody = "myResponse";
+    var sResponseHeaders = "Content-Type: application/json;charset=utf-8";
+    var iResponseTime = 480;
+    var sSapStatistics = "";
+
+    this._oAssertion.assert(iStatus, sResponseBody, sResponseHeaders, iResponseTime, sSapStatistics);
+    assert.equal(this._oAssertion.getResultReady(), true, "Result is ready");
+    assert.equal(this._oAssertion.getResult(), false, "Result false");
+    
+    iStatus = 201;
+    this._oAssertion.assert(iStatus, sResponseBody, sResponseHeaders, iResponseTime, sSapStatistics);
+    assert.equal(this._oAssertion.getResult(), true, "Result true");
+    
+    //TODO test more cases
 });
 
-QUnit.test("_opEquals", 1, function(assert) {
-	this._oAssertion._opEquals();
+QUnit.test("_opEquals", 2, function(assert) {
+    assert.equal(this._oAssertion._opEquals("ab", "ab"), true, "ok");
+    assert.equal(this._oAssertion._opEquals("a", "ab"), false, "ok");
 });
 
-QUnit.test("_opEqualsNot", 1, function(assert) {
-	this._oAssertion._opEqualsNot();
+QUnit.test("_opEqualsNot", 2, function(assert) {
+    assert.equal(this._oAssertion._opEqualsNot("ab", "ab"), false, "ok");
+    assert.equal(this._oAssertion._opEqualsNot("a", "ab"), true, "ok");
 });
 
-QUnit.test("_opLess", 1, function(assert) {
-	this._oAssertion._opLess();
+QUnit.test("_opLess", 2, function(assert) {
+    assert.equal(this._oAssertion._opLess("100", "120"), true, "ok");
+    assert.equal(this._oAssertion._opLess("120", "100"), false, "ok");
 });
 
-QUnit.test("_opLessOrEqual", 1, function(assert) {
-	this._oAssertion._opLessOrEqual();
+QUnit.test("_opLessOrEqual", 3, function(assert) {
+    assert.equal(this._oAssertion._opLessOrEqual("100", "120"), true, "ok");
+    assert.equal(this._oAssertion._opLessOrEqual("120", "100"), false, "ok");
+    assert.equal(this._oAssertion._opLessOrEqual("100", "100"), true, "ok");
 });
 
-QUnit.test("_opGreater", 1, function(assert) {
-	this._oAssertion._opGreater();
+QUnit.test("_opGreater", 2, function(assert) {
+    assert.equal(this._oAssertion._opGreater("100", "120"), false, "ok");
+    assert.equal(this._oAssertion._opGreater("120", "100"), true, "ok");
 });
 
-QUnit.test("_opGreaterOrEqual", 1, function(assert) {
-	this._oAssertion._opGreaterOrEqual();
+QUnit.test("_opGreaterOrEqual", 3, function(assert) {
+    assert.equal(this._oAssertion._opGreaterOrEqual("100", "120"), false, "ok");
+    assert.equal(this._oAssertion._opGreaterOrEqual("120", "100"), true, "ok");
+    assert.equal(this._oAssertion._opGreaterOrEqual("100", "100"), true, "ok");
 });
 
-QUnit.test("_opExists", 1, function(assert) {
-	this._oAssertion._opExists();
+QUnit.test("_opExists", 2, function(assert) {
+    assert.equal(this._oAssertion._opExists("llo wo", undefined), true, "ok");
+    assert.equal(this._oAssertion._opExists("", undefined), false, "ok");
 });
 
-QUnit.test("_opExistsNot", 1, function(assert) {
-	this._oAssertion._opExistsNot();
+QUnit.test("_opExistsNot", 2, function(assert) {
+    assert.equal(this._oAssertion._opExistsNot("llo wo", undefined), false, "ok");
+    assert.equal(this._oAssertion._opExistsNot("", undefined), true, "ok");
 });
 
-QUnit.test("_opContains", 1, function(assert) {
-	this._oAssertion._opContains();
+QUnit.test("_opContains", 2, function(assert) {
+    assert.equal(this._oAssertion._opContains("hello world", "llo wo"), true, "ok");
+    assert.equal(this._oAssertion._opContains("hello world", "wirld"), false, "ok");
 });
 
-QUnit.test("_opContainsNot", 1, function(assert) {
-	this._oAssertion._opContainsNot();
+QUnit.test("_opContainsNot", 2, function(assert) {
+    assert.equal(this._oAssertion._opContainsNot("hello world", "llo wo"), false, "ok");
+    assert.equal(this._oAssertion._opContainsNot("hello world", "wirld"), true, "ok");
 });
 
--- tests/unit/util/Controller.js
+++ tests/unit/util/Controller.js
@@ -1,8 +1,10 @@
 jQuery.sap.require("projectX.util.Controller");
 
+
 QUnit.module("util/Controller", {
     setup: function() {
       this._oController = new projectX.util.Controller();
+      
     },
     teardown: function() {
 			
@@ -15,48 +17,70 @@ QUnit.test("Instantiation", 1, function(assert) {
 });
 
 QUnit.test("_baseOnInit", 1, function(assert) {
+    var spy = sinon.spy();
+    this._oController._hookViewEvents = spy;
 	this._oController._baseOnInit();
+    assert.ok(spy.called, "hookViewEvents called");
 });
 
 QUnit.test("onInit", 1, function(assert) {
+    var spy = sinon.spy();
+    this._oController._hookViewEvents = spy;
 	this._oController.onInit();
+    assert.ok(spy.called, "hookViewEvents called");
 });
 
-QUnit.test("onExit", 1, function(assert) {
-	this._oController.onExit();
+QUnit.test("onExit", 1, function(assert) {    
+    this._oController._busyDialog = { destroy : sinon.spy()};
+    this._oController.onExit();
+    assert.ok(this._oController._busyDialog.destroy.called ,"busydialog destroyed");
 });
 
 QUnit.test("_hookViewEvents", 1, function(assert) {
-	this._oController._hookViewEvents();
+	var fAddViewEventDelegateSpy = sinon.spy();
+    this._oController.getView = function () {
+        return {
+            addEventDelegate : fAddViewEventDelegateSpy
+        };
+    }
+    
+    this._oController._hookViewEvents();
+    assert.ok(fAddViewEventDelegateSpy.callCount === 4, "events hooked");
 });
 
-QUnit.test("onBeforeHide", 1, function(assert) {
+QUnit.test("onBeforeHide", 2, function(assert) {
+    assert.ok(!this._oController._bIsViewHidden, "unknown hide state");
 	this._oController.onBeforeHide();
+    assert.ok(this._oController._bIsViewHidden === true, "hide state");
 });
 
-QUnit.test("onAfterHide", 1, function(assert) {
+QUnit.test("onAfterHide", 0, function(assert) {
+    //nothing happens here
 	this._oController.onAfterHide();
 });
 
-QUnit.test("onBeforeShow", 1, function(assert) {
+QUnit.test("onBeforeShow", 2, function(assert) {
+    assert.ok(!this._oController._bIsViewHidden, "unknown hide state");
 	this._oController.onBeforeShow();
+    assert.ok(this._oController._bIsViewHidden === false, "no hide state");
 });
 
-QUnit.test("onAfterShow", 1, function(assert) {
+QUnit.test("onAfterShow", 0, function(assert) {
+    //nothing to test
 	this._oController.onAfterShow();
 });
 
-QUnit.test("getEventBus", 1, function(assert) {
-	this._oController.getEventBus();
-});
+// QUnit.test("getEventBus", 1, function(assert) {
+// 	this._oController.getEventBus();
+// });
+// 
+// QUnit.test("getRouter", 1, function(assert) {
+// 	this._oController.getRouter();
+// });
 
-QUnit.test("getRouter", 1, function(assert) {
-	this._oController.getRouter();
-});
-
-QUnit.test("getComponent", 1, function(assert) {
-	this._oController.getComponent();
-});
+// QUnit.test("getComponent", 1, function(assert) {
+// 	this._oController.getComponent();
+// });
 
 QUnit.test("updateMasterList", 1, function(assert) {
 	this._oController.updateMasterList();
