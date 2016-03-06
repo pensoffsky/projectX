438d08774987c6e1ffb5a08010d173e5f7a745d1
Kuesters, Elmar <pensoffsky@gmx.net>
2016-03-06 21:52:37 +0100

    implemented opa5 test: new sequence, add request from dialog

--- projectX/test-resources/integration/pages/Master.js
+++ projectX/test-resources/integration/pages/Master.js
@@ -12,16 +12,31 @@ function (Opa5, Common) {
 			baseClass : Common,
 			actions : {
 				iTapOnSequenceTab : function () {
-					
+					return this.triggerTapOnCtrlWithID(sViewName, "idButtonSequences");
+				},
+				
+				iTapOnRequestTab : function () {
+					return this.triggerTapOnCtrlWithID(sViewName, "idButtonRequests");
 				},
 				
 				iTapOnNewSequence : function () {
-					
+					return this.triggerTapOnCtrlWithID(sViewName, "testingIDNewSequence");
 				}
 			}, 
 			assertions : {
 				iSeeSequenceWithName : function (sSequenceName) {
-					
+					return this.waitFor({
+						viewName : sViewName,
+						controlType: "sap.m.Text",
+						matchers : [new sap.ui.test.matchers.PropertyStrictEquals({
+								name : "text",
+								value: sSequenceName
+						  })],
+						success : function () {
+							ok(true, "sequence with name ok: " + sSequenceName);
+						},
+						errorMessage : "Did not find list item control with text: " + sSequenceName
+					});
 				},
 			}
 		}
--- projectX/test-resources/integration/pages/Sequence.js
+++ projectX/test-resources/integration/pages/Sequence.js
@@ -12,24 +12,57 @@ function (Opa5, Common) {
 			baseClass : Common,
 			actions : {
 				iTapOnSelectRequestDialogOK : function () {
-					
+					return this.triggerTapOnDialogButtonWithText("OK");
 				},
 				
 				iSelectRequestWithName : function (sRequestName) {
-					
+					return this.waitFor({
+						searchOpenDialogs: true,
+						controlType: "sap.m.StandardListItem",
+						matchers : new sap.ui.test.matchers.PropertyStrictEquals({
+								name : "title",
+								value: sRequestName
+						  }),
+						success : function (aListItems) {
+							aListItems[0].$().trigger("tap");
+							ok(true, "triggered tap on list item with text: " + sRequestName);
+						},
+						errorMessage : "Did not find list item with text: " + sRequestName
+					});
 				},
 				
 				iTapOnAddRequests : function () {
-					
+					return this.triggerTapOnCtrlWithID(sViewName, "testingIDAddRequest");
 				},
 			},
 			assertions : {
-				iSeeRequestWithName : function (sRequestName) {
-					
+				iSeeRequestsInList : function () {
+					return this.waitFor({
+						viewName : sViewName,
+						id: "idSelectedRequestsTable",
+						matchers : new sap.ui.test.matchers.AggregationFilled({
+								name : "items"
+						  }),
+						success : function () {
+							ok(true, "found request in table");
+						},
+						errorMessage : "Did not find request in table: "
+					});
 				},
 				
 				iSeeSequenceName : function (sSequenceName) {
-					
+					return this.waitFor({
+						viewName : sViewName,
+						id: "testingIDSequenceName",
+						matchers : new sap.ui.test.matchers.PropertyStrictEquals({
+								name : "value",
+								value: sSequenceName
+						  }),
+						success : function () {
+							ok(true, "found sequence name : " + sSequenceName);
+						},
+						errorMessage : "Did not find list item with text: " + sSequenceName
+					});
 				},
 			}
 		}
